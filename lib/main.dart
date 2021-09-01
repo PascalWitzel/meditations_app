60f42985ef6faf1617344f889cd3f600b67042e7
[Pascal] <[passi200999@gmail.com]>
2021-09-01 14:08:34 +0200

    Animation geändert und angepasst

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meditations_app/screens/welcome_screen.dart';
 import 'package:meditations_app/themes/theme_notifier.dart';
@@ -11,11 +12,11 @@ void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String themeString = prefs.getString('theme') ?? "orangeTheme";
   ThemeData theme;
-  if(themeString == "orangeTheme"){
+  if (themeString == "orangeTheme") {
     theme = orangeTheme;
-  } else if(themeString == "greenTheme"){
+  } else if (themeString == "greenTheme") {
     theme = greenTheme;
-  } else if(themeString == "purpleTheme"){
+  } else if (themeString == "purpleTheme") {
     theme = purpleTheme;
   } else {
     theme = blueTheme;
@@ -30,25 +31,43 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     final themeNotifier = Provider.of<ThemeNotifier>(context);
     themeNotifier.getThemeSP();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: themeNotifier.getTheme(),
-      home: AnimatedSplashScreen(
-        duration: 3000,
-        splash: Image.asset(
-            'assets/images/forground.png'
-        ),
-        nextScreen: WelcomeScreen(),
-        splashTransition: SplashTransition.scaleTransition,
-        backgroundColor: Colors.white70,
-
-      )
-
-    );
+        title: 'Flutter Demo',
+        theme: themeNotifier.getTheme(),
+        home: Center(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Theme.of(context).primaryColor,
+                    Theme.of(context).accentColor,
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background.png"),
+                  fit: BoxFit.fitWidth,
+                )),
+            child: AnimatedSplashScreen(
+              duration: 2000,
+              splash: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/forground.png"),
+                  ),
+                ),
+              ),
+              splashIconSize: 275,
+              nextScreen: WelcomeScreen(),
+              splashTransition: SplashTransition.scaleTransition,
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+        ));
   }
 }
