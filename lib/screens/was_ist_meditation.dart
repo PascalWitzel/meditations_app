b7cec93c9ecab009a80f29431d4e8bd2f7d83b04
[Pascal] <[passi200999@gmail.com]>
2021-09-08 11:31:25 +0200

    hintergrundbilder bei was ist meditation und wie geht meditation eingefügt

--- lib/screens/was_ist_meditation.dart
+++ lib/screens/was_ist_meditation.dart
@@ -1,38 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'appbar.dart';
 
 class wasIstMeditation extends StatelessWidget {
 
+  List<String> images = [
+    "assets/images/Download.jpg",
+    "assets/images/Steinturm02.jpg",
+    "assets/images/Meditation Berg.jpg",
+    "assets/images/Meditation Ozean.jpg",
+  ];
+  var random = new Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbarback(context, "Was ist Meditation"),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Text(
-                "Der Begriff Meditation lässt sich vom lateinischen 'meditatio' (das Nachdenken über) bzw. 'medio' (die Mitte) ableiten und steht unter anderem für mentale Versenkung. Meditation ist eine spirituelle Praxis, die durch Konzentrationsübungen tiefe Entspannung und veränderte Bewusstseinszustände erreichen kann.",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
-                  fontSize: 20,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.2), BlendMode.dstATop),
+            image: AssetImage(images[random.nextInt(images.length - 1)]),
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Text(
+                  "Der Begriff Meditation lässt sich vom lateinischen 'meditatio' (das Nachdenken über) bzw. 'medio' (die Mitte) ableiten und steht unter anderem für mentale Versenkung. Meditation ist eine spirituelle Praxis, die durch Konzentrationsübungen tiefe Entspannung und veränderte Bewusstseinszustände erreichen kann.",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-                child: Image(image: AssetImage("assets/images/Meditation Berg.jpg")),
-              ),
-              Text(
-                "Ziel ist die Stille oder auch innere Leere, das Freisein von jeglichen Gedanken. In religiöser Hinsicht will man die Erleuchtung erlangen, das 'Eins-Sein mit dem Ganzen'.",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
-                  fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: Image(image: AssetImage("assets/images/Meditation Berg.jpg")),
                 ),
-              ),
-            ],
+                Text(
+                  "Ziel ist die Stille oder auch innere Leere, das Freisein von jeglichen Gedanken. In religiöser Hinsicht will man die Erleuchtung erlangen, das 'Eins-Sein mit dem Ganzen'.",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/wie_geht_meditation.dart
+++ lib/screens/wie_geht_meditation.dart
@@ -1,51 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'appbar.dart';
 
 class wieGehtMeditation extends StatelessWidget {
 
+  List<String> images = [
+    "assets/images/Download.jpg",
+    "assets/images/Steinturm02.jpg",
+    "assets/images/Meditation Berg.jpg",
+    "assets/images/Meditation Ozean.jpg",
+  ];
+  var random = new Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbarback(context, "Wie geht Meditation"),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                Text(
-                  "Es gibt zwei verschiedene (äußere) Arten der Meditation: Bei der passiven Meditation sitzt oder liegst du ohne dich zu bewegen. Bei der aktiven Meditation hingegen führst du körperliche Übungen aus. Das kann einfaches Gehen sein, aber ebenso Yoga und bestimmte Kampfkunststile umfassen. Auch das Beten oder Aufsagen von Affirmation (Glaubenssätze) und Mantras gehört dazu.",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
-                    fontSize: 20,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.2), BlendMode.dstATop),
+            image: AssetImage(images[random.nextInt(images.length - 1)]),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Es gibt zwei verschiedene (äußere) Arten der Meditation: Bei der passiven Meditation sitzt oder liegst du ohne dich zu bewegen. Bei der aktiven Meditation hingegen führst du körperliche Übungen aus. Das kann einfaches Gehen sein, aber ebenso Yoga und bestimmte Kampfkunststile umfassen. Auch das Beten oder Aufsagen von Affirmation (Glaubenssätze) und Mantras gehört dazu.",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-                  child: Image(image: AssetImage("assets/images/yogasunset.jpg")),
-                ),
-                Text(
-                  "Essenzieller Kern jeder Form der Meditation ist die Bündelung der Aufmerksamkeit. Dabei kannst du dich auf verschiedene Objekte konzentrieren: deine Atmung, körperliche Empfindungen, Emotionen, Gerüche, Geräusche oder ein Bild vor deinem geistigen Auge.",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
-                    fontSize: 20,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                    child: Image(image: AssetImage("assets/images/yogasunset.jpg")),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-                  child: Image(image: AssetImage("assets/images/Steinturm03.jpg")),
-                ),
-                Text(
-                  "Das Ziel der Meditation ist es, deinen Geist zu fokussieren und dadurch zu beruhigen. Wenn du über einen längeren Zeitraum regelmäßig meditierst, wirst du den Fortschritt spüren. Gesundheit und Immunsystem werden gestärkt, du fühlst dich weniger gestresst und bist ausgeglichener. Ein weiterer positiver Effekt: Konzentrationsfähigkeit und Kreativität steigen.",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
-                    fontSize: 20,
+                  Text(
+                    "Essenzieller Kern jeder Form der Meditation ist die Bündelung der Aufmerksamkeit. Dabei kannst du dich auf verschiedene Objekte konzentrieren: deine Atmung, körperliche Empfindungen, Emotionen, Gerüche, Geräusche oder ein Bild vor deinem geistigen Auge.",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                    child: Image(image: AssetImage("assets/images/Steinturm03.jpg")),
+                  ),
+                  Text(
+                    "Das Ziel der Meditation ist es, deinen Geist zu fokussieren und dadurch zu beruhigen. Wenn du über einen längeren Zeitraum regelmäßig meditierst, wirst du den Fortschritt spüren. Gesundheit und Immunsystem werden gestärkt, du fühlst dich weniger gestresst und bist ausgeglichener. Ein weiterer positiver Effekt: Konzentrationsfähigkeit und Kreativität steigen.",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: Theme.of(context).textTheme.headline1.fontFamily,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
